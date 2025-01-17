777f3f5bed36f5ea59ed40a789e727a63c4ad574
Edward Lance Arellano Lorilla <edwardlorilla1986@gmail.com>
2025-01-17 19:56:54 +0800

    Update fetch_rss.py

--- fetch_rss.py
+++ fetch_rss.py
@@ -5,8 +5,11 @@ import os
 import random
 import subprocess
 import smtplib
-from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
 import os
 from datetime import datetime
 import requests
