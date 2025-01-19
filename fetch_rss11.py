16afc43a867e3e1c2546508f7f7ff28da67ae49d
Edward Lance Arellano Lorilla <edwardlorilla1986@gmail.com>
2025-01-19 19:10:16 +0800

    Update fetch_rss11.py

--- fetch_rss11.py
+++ fetch_rss11.py
@@ -33,7 +33,7 @@ if os.path.exists(seed_file):
         seed = int(file.read().strip()) + 1
 else:
     seed = 1
-rss_url = 'https://ourdailynewsonline.com/feed/'
+rss_url = 'https://www.inquirer.net/fullfeed/'
 feed = feedparser.parse(rss_url)
 
 # Parse the RSS feed entries
