d90a2c984d6fd4b474b6f4a901a1be62ed9da6fd
Edward Lance Arellano Lorilla <edwardlorilla1986@gmail.com>
2025-01-19 19:00:26 +0800

    Update fetch_rss5.py

--- fetch_rss5.py
+++ fetch_rss5.py
@@ -15,7 +15,7 @@ import json
 import os
 from datetime import datetime
 
-rss_url = 'https://www.manilatimes.net/opinion/feed/'
+rss_url = 'https://unbox.ph/feed/'
 feed = feedparser.parse(rss_url)
 
 # Parse the RSS feed entries
